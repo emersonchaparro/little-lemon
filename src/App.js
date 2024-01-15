68ade05ae421b5c4609acf3303db9c79b58e9431
emersonchaparro <emersonchaparroampa@gmail.com>
2024-01-15 11:13:58 -0500

    basic nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div>
-      Little lemon
-    </div>
+    <>
+    <Nav />
+    <Header />
+    <Main/>
+    <Footer />
+    </>
   );
 }
 
